7398a3907aefb2eb593e10c36ce9c7a6556f5236
Ivan John Nacion <kunnivan@gmail.com>
2023-10-11 13:01:11 +0800

    Update 10/11/2023
    Adding animation in javascript and contact section.

--- script.js
+++ script.js
@@ -6,7 +6,36 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+
+    //slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    //Typing Animation Script
+    var typed = new Typed(".typing-home", {
+        strings: ["Software Developer", "Graphic Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-about", {
+        strings: ["Software Developer", "Graphic Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
 
     //toggle menu/navbar script
    $('.menu-btn').click(function(){
